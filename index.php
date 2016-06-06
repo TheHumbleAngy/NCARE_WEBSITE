9cff82d16fa4fc4bf25d0cc78ab99b43b963cbef
Ange Kouakou <angemarius@gmail.com>
2016-06-06 15:35:25 +0000

    Update 1.0
    MAJ de l'entete, du menu, et du bas de page etc...

--- index.php
+++ index.php
@@ -4,53 +4,65 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <link href="css/js-image-slider.css" rel="stylesheet" type="text/css"/>
+    <!--<link href="css/js-image-slider.css" rel="stylesheet" type="text/css"/>
     <script src="js/js-image-slider.js" type="text/javascript"></script>
 
     <link href="css/bootstrap.css" rel="stylesheet" type="text/css"/>
     <link href="css/bootstrap-responsive.css" rel="stylesheet" type="text/css"/>
 
-    <link href="css/mnwrp.css" rel="stylesheet" type="text/css" />
+    <link href="css/mnwrp.css" rel="stylesheet" type="text/css"/>
 
     <link href="css/style.css" rel="stylesheet" type="text/css"/>
     <link href="css/screen.css" rel="stylesheet" type="text/css"/>
 
     <script src="js/jquery.js"></script>
     <script src="js/vsmenu.js"></script>
-    <link href="css/vsmenu_.css" type="text/css" rel="stylesheet"/>
+    <link href="css/vsmenu_.css" type="text/css" rel="stylesheet"/>-->
 
-    <style type="text/css">
-        body {
-            background: #e0f7fe url(images/pattern_3.png) fixed !important;
-        }
-    </style>
+    <link type="text/css" href="css_js/bootstrap-3.3.4-dist/css/bootstrap.css" rel="stylesheet">
+    <link type="text/css" href="css_js/bootstrap-3.3.4-dist/css/bootstrap-theme.css" rel="stylesheet">
+    <link type="text/css" href="css_js/stylish.css" rel="stylesheet">
+    <link type="text/css" href="css_js/mnwrp.css" rel="stylesheet"/>
+
+    <script src="css_js/js-image-slider.js" type="text/javascript"></script>
+    <script src="css_js/bootstrap-3.3.4-dist/js/jquery-1.11.3.js"></script>
+    <script src="css_js/bootstrap-3.3.4-dist/js/bootstrap.js"></script>
 
     <title>NCA Re</title>
 </head>
-<body>
+<body class="arriere_plan">
 <div class="outer">
     <div class="container">
-        <div class="sb sb_blue">
-            <table width=100% style="margin: 3px">
-                <tr>
-                    <td width="80%"><img src="images/logo1-resized.png" alt="logo"/></td>
-                    <td width="20%" align="center"><a href="index.php?page=accueil" class="police">ACCUEIL</a> | <a
-                            href="index.php?page=sitemap" class="police">SITEMAP</a> | <a href="index.php?page=contact"
-                                                                                          class="police">CONTACT</a>
-                    </td>
-                </tr>
-            </table>
+        <div class="row hautdepage">
+            <div class="col-md-12 nopadding">
+                <div class="row">
+                    <div class="col-md-3 nopadding">
+                        <div class="logo_entete">
+                            <a href="">
+                                <img src="images/logo1-resized.png" alt="logo"/>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-md-offset-6" style="color: #01addd; text-align: right; top: -5px">
+                        <p style="padding-top: 30px; margin: 0">
+                            <a href="index.php?page=accueil">ACCUEIL</a> |
+                            <a href="index.php?page=sitemap">SITEMAP</a> |
+                            <a href="index.php?page=contact">CONTACT</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
 
         <!-- Image animation -->
         <div id="sliderframe">
             <div id="slider">
-                <img src="images/slide-14.jpg" name="slide1" width="1024" height="300">
-                <img src="images/slide-18.jpg" name="slide2" width="1024" height="300">
-                <img src="images/slide-16.jpg" name="slide3" width="1024" height="300">
-                <img src="images/slide-17.jpg" name="slide4" width="1024" height="300">
-                <img src="images/slide-12.jpg" name="slide5" width="1024" height="300">
-                <img src="images/slide-15.jpg" name="slide6" width="1024" height="300">
+                <img src="images/slide-14.jpg" name="slide1" width="100%" height="250">
+                <img src="images/slide-18.jpg" name="slide2" width="100%" height="250">
+                <img src="images/slide-16.jpg" name="slide3" width="100%" height="250">
+                <img src="images/slide-17.jpg" name="slide4" width="100%" height="250">
+                <img src="images/slide-12.jpg" name="slide5" width="100%" height="250">
+                <img src="images/slide-15.jpg" name="slide6" width="100%" height="250">
             </div>
         </div>
         <!-- End of Image animation -->
@@ -87,110 +99,74 @@
     </div>
 
     <footer>
-        <div class="container">
-            <div class="fcol f_col_1">
-                <!-- widget contacts -->
-                <div class="widget-container widget_contact">
-                    <h3 class="widget-title">Nous contacter:</h3>
-                    <div class="inner">
-                        <div class="contact-address">
-                            <div class="address">
-                                <p>Nouvelle Compagnie Africaine de Réassurance</p>
-                                <p>Rez-de-chaussée Immeuble les Harmonies Abidjan Plateau</p>
-                                <p>01 BP 5962 Abidjan 01 Côte d'Ivoire</p>
-                            </div>
-                            <div class="phone"><em>Tel:</em> <span>00 225 20 21 92 06 <br/>
-									</span> Fax: <span>00 225 20 21 90 73</span></div>
-                            <div class="mail"><em>Email:</em> <a href="mailto:infos@nca-re.net"
-                                                                 style="outline: none;">infos@nca-re.net</a></div>
-                        </div>
-                    </div>
+        <div class="container" style="padding: 50px 0 40px; width: 80%">
+            <div class="row">
+                <div class="col-md-4">
+                    <h3 style="margin-top: 10px;">Nous contacter:</h3>
+                    <address>
+                        <strong>Nouvelle Compagnie Africaine de Réassurance</strong>
+                        <p>Rez-de-chaussée Immeuble les Harmonies Abidjan Plateau</p>
+                        <p>01 BP 5962 Abidjan 01 Côte d'Ivoire</p>
+                        <br>
+                        <abbr title="Téléphone">Tel:</abbr> 00 225 20 21 92 06 <br/>
+                        Fax: 00 225 20 21 90 73 <br/>
+                        E-mail: <a href="mailto:infos@nca-re.net" style="text-decoration: none;">infos@nca-re.net</a>
+                    </address>
                 </div>
-                <!--/ widget contacts -->
-            </div>
-            <!--/ f_col_1 -->
-
-            <div class="fcol f_col_2">
-                <!-- widget widget_nav_menu -->
-                <div class="widget-container widget_nav_menu">
-                    <h3 class="widget-title">A Propos:</h3>
-                    <ul class="iconless">
-                        <li class="even"><a href="index.php?page=accueil"
-                                            style="outline: none;"><span>Profile</span></a>
+                <div class="col-md-2">
+                    <h4>A Propos:</h4>
+                    <ul class="list-unstyled">
+                        <li>
+                            <a href="index.php?page=accueil" style="text-decoration: none;"><span>Profile</span></a>
                         </li>
-                        <li><a href="#" style="outline: none;"><span>Historique</span></a></li>
-                        <li class="even"><a href="#" style="outline: none;"><span>Mots du Directeur général</span></a>
+                        <li>
+                            <a href="#" style="text-decoration: none;"><span>Chiffres Clés</span></a>
                         </li>
-                        <li><a href="index.php?page=mission"
-                               style="outline: none;"><span>Mission</span></a></li>
-                        <li><a href="index.php?page=souscriptions" style="outline: none;"><span>Nos Souscriptions</span></a>
-                        </li>
-                        <li><a href="index.php?page=equipe"
-                               style="outline: none;"><span>Structure Organisationnelle</span></a>
+                        <li>
+                            <a href="#" style="text-decoration: none;"><span>Mots du Directeur général</span></a>
                         </li>
                     </ul>
                 </div>
-                <!-- widget widget_nav_menu -->
-            </div>
-            <!--/ f_col_2 -->
-
-            <div class="fcol f_col_3">
-
-                <!-- widget widget_nav_menu -->
-                <div class="widget-container widget_nav_menu">
-                    <h3 class="widget-title">Réassurance:</h3>
-                    <ul class="iconless">
-                        <li class="even"><a href="#" style="outline: none;"><span>Vue d'ensemble</span></a>
+                <div class="col-md-2">
+                    <h4>Politique de Souscription</h4>
+                    <ul class="list-unstyled">
+                        <li>
+                            <a href="#" style="text-decoration: none;"><span>Territorialité</span></a>
                         </li>
-                        <li><a href="#" style="outline: none;"><span>Stratégie(s)</span></a></li>
-                    </ul>
-                </div>
-                <!-- widget widget_nav_menu -->
-
-            </div>
-            <!--/ f_col_3 -->
-
-            <div class="fcol f_col_4">
-
-                <!-- widget widget_categories -->
-                <div class="widget-container widget_categories">
-                    <h3 class="widget-title">Activités et Services:</h3>
-                    <ul class="iconless">
-                        <li class="even"><a href="#"
-                                            style="outline: none;"><span>Non-life Reinsurance</span></a></li>
-                        <li><a href="#" style="outline: none;"><span>Réassurance Vie</span></a></li>
-                        <li class="even"><a href="#" style="outline: none;"><span>Risks Solution</span></a>
+                        <li>
+                            <a href="#" style="text-decoration: none;"><span>Branches</span></a>
                         </li>
-                        <li><a href="#" style="outline: none;"><span>Sinistres</span></a></li>
-                    </ul>
-                </div>
-                <!-- widget widget_categories -->
-
-            </div>
-            <!--/ f_col_4 -->
-
-            <div class="fcol f_col_5">
-
-                <!-- widget widget_nav_menu -->
-                <div class="widget-container widget_nav_menu">
-                    <h3 class="widget-title">Marché des Capitaux:</h3>
-                    <ul class="iconless">
-                        <li><a href="#"
-                               style="outline: none;"><span>Philosophie d'Investissement</span></a></li>
-                        <li class="even"><a href="#" style="outline: none;"><span>Portefeuille d'Investissements</span></a>
-                        </li>
-                        <li><a href="#" style="outline: none;"><span>Instruments à Révenu Fixes</span></a>
-                        </li>
-                        <li><a href="#" style="outline: none;"><span>Actions Cotées</span></a></li>
-                        <li><a href="#" style="outline: none;"><span>Fonds Privés</span></a></li>
-                        <li><a href="#" style="outline: none;"><span>Investissements Alternatifs</span></a>
+                        <li>
+                            <a href="#" style="text-decoration: none;"><span>Capacité</span></a>
                         </li>
                     </ul>
                 </div>
-                <!-- widget widget_nav_menu -->
-
+                <div class="col-md-2">
+                    <h4>Partenaires</h4>
+                    <ul class="list-unstyled">
+                        <li>
+                            <a href="#" style="text-decoration: none;"><span>Courtiers</span></a>
+                        </li>
+                        <li>
+                            <a href="#" style="text-decoration: none;"><span>Rétrocessionnaires</span></a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-md-2">
+                    <h4>Dirigeants:</h4>
+                    <ul class="list-unstyled">
+                        <li>
+                            <a href="#" style="text-decoration: none;"><span>Conseil d'Administration</span></a>
+                        </li>
+                        <li>
+                            <a href="#" style="text-decoration: none;"><span>Direction Générale</span></a>
+                        </li>
+                        <li>
+                            <a href="#" style="text-decoration: none;"><span>Autres Responsables</span></a>
+                        </li>
+                    </ul>
+                </div>
             </div>
-
         </div>
     </footer>
 </div>
--- mnwrp.php
+++ mnwrp.php
@@ -1,26 +1,23 @@
 ﻿<nav id="main-nav">
     <ul id="nav-primary">
-        <li><a href="index.php">Accueil</a></li>
-        <li><a href="index.php?page=accueil">A Propos</a>
-
+<!--        TODO: Les pourcentage de largeur des <li> sont à MAJ selon qu'un est ajouté ou supprimé -->
+        <li style="width: 8.83%">
+            <a href="index.php">Accueil</a>
+        </li>
+        <li style="width: 10%">
+            <a href="index.php?page=accueil">A Propos</a>
             <ul class="subnav">
                 <div class="triangle"></div>
                 <li><a href="#">Profile</a>
                 </li>
-                <li><a href="#">Histoire</a>
+                <li><a href="#">Chiffres Clés</a>
                 </li>
                 <li><a href="#">Mot du Directeur Général</a>
                 </li>
-                <li><a href="index.php?page=mission">Mission</a>
-                </li>
-                <li><a href="#">Nos Souscriptions</a>
-                </li>
-                <li><a href="#">Structure Organisationnelle</a>
-                </li>
             </ul>
         </li>
-        <li><a href="#">Réassurance</a>
-
+        <li style="width: 12.48%">
+            <a href="#">Réassurance</a>
             <ul class="subnav">
                 <div class="triangle"></div>
                 <li><a href="#">Vue d'ensemble</a>
@@ -29,41 +26,45 @@
                 </li>
             </ul>
         </li>
-        <li><a href="#">Activités & Services</a>
-
+        <li class="justified">
+            <a href="#">Politique de Souscription</a>
             <ul class="subnav">
                 <div class="triangle"></div>
-                <li><a href="#">Non-Life Reinsurance</a>
+                <li><a href="#">Territorialité</a>
                 </li>
-                <li><a href="#">Réassurance Vie</a>
+                <li><a href="#">Branches</a>
                 </li>
-                <li><a href="#">Réassurance Santé</a>
-                </li>
-                <li><a href="#">Risks Solution</a>
-                </li>
-                <li><a href="#">Sinistres</a>
+                <li><a href="#">Capacité</a>
                 </li>
             </ul>
         </li>
-        <li><a href="#">Marchés des Capitaux</a>
-
+        <li style="width: 11.57%">
+            <a href="#">Partenaires</a>
             <ul class="subnav">
                 <div class="triangle"></div>
-                <li><a href="#">Philosophie d'Investissement</a>
+                <li><a href="#">Courtiers</a>
                 </li>
-                <li><a href="#">Portefeuille d'Investissements</a>
-                </li>
-                <li><a href="#">Instruments à Revenus Fixes</a>
-                </li>
-                <li><a href="#">Actions Cotées</a>
-                </li>
-                <li><a href="#">Fonds Privés</a>
-                </li>
-                <li><a href="">Investissements Alternatifs</a>
+                <li><a href="#">Rétrocessionnaires</a>
                 </li>
             </ul>
         </li>
-        <li><a href="#">News</a></li>
-        <li><a href="#">Contact</a></li>
+        <li class="justified">
+            <a href="#">Apports Supplémentaires</a>
+        </li>
+        <li style="width: 10.65%">
+            <a href="#">Dirigeants</a>
+            <ul class="subnav">
+                <div class="triangle"></div>
+                <li><a href="#">Conseil d'Administration</a>
+                </li>
+                <li><a href="#">Direction Générale</a>
+                </li>
+                <li><a href="#">Autres Responsables</a>
+                </li>
+            </ul>
+        </li>
+        <li style="width: 9.44%">
+            <a href="#">Contact</a>
+        </li>
     </ul>
 </nav>
